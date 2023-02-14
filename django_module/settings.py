031e4e68a86a7f5a3db3ccbd728904c1eb79dd46
Пользователь <dm.stebenev@gmail.com>
2023-02-14 12:39:17 +0200

    add media files

--- django_module/settings.py
+++ django_module/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,6 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'mysite.MyUser'
 # LOGOUT_REDIRECT_URL = "login"
 # LOGIN_REDIRECT_URL = "home"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 CACHES = {
